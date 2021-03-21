7376204dd002fd2d7a537ec25cf513ac555e8b38
AnimeshAnand1999 <animeanand99@gmail.com>
2021-03-21 12:31:01 +0530

    commit three

---
+++ close.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include"connect.h"
+using namespace std;
+void close(vector<account*>& vec, int accno)
+{
+	int i = accno - 1001;
+	account* temp = new account();
+	temp->next = vec[i];
+	vec[i] = temp;
+	cout << "account is closed" << endl;
+}
